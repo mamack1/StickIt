508b0f341d4406ae5ce718a6a4e996a8c6190fbc
mamack1 <mamack1@dmacc.edu>
2024-07-24 12:36:30 -0500

    modified and deleted files to create note, created dashboard.ts that creates the note element and calls it when button is pressed, begun working on making it draggable

---
+++ js/dashboard.js
@@ -0,0 +1,47 @@
+"use strict";
+// note element
+function createNewNote() {
+    console.log("createnewnote function called in note.ts!");
+    const note = document.createElement("div");
+    note.className = "note";
+    note.innerHTML = `
+    <textarea class="note-content" placeholder="Type your note here!"></textarea>
+    <button class="close-note">X</button>
+    `;
+    // makeDraggable(note);
+    document.body.appendChild(note);
+    const closeButton = note.querySelector(".close-note");
+    closeButton.addEventListener("click", () => {
+        document.body.removeChild(note);
+    });
+    console.log("Note window opened");
+}
+//draggable
+// function makeDraggable(element: HTMLElement) {
+// 	let offsetX: number,
+// 		offsetY: number,
+// 		isDragging: boolean = false;
+// 	element.addEventListener("mousedown", (event) => {
+// 		offsetX = event.clientX - element.getBoundingClientRect().left;
+// 		offsetY = event.clientY - element.getBoundingClientRect().top;
+// 		isDragging = true;
+// 	});
+// 	document.addEventListener("mousemove", (event) => {
+// 		if (isDragging) {
+// 			element.style.left = `${event.clientX - offsetX}px`;
+// 			element.style.top = `${event.clientY - offsetY}px`;
+// 		}
+// 	});
+// 	document.addEventListener("mouseup", () => {
+// 		isDragging = false;
+// 	});
+// }
+document.addEventListener("DOMContentLoaded", () => {
+    const createNote = document.getElementById("createNote");
+    if (createNote) {
+        createNote.addEventListener("click", () => {
+            console.log("note function called in popupMM.ts!");
+            createNewNote();
+        });
+    }
+});

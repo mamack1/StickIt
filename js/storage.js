1b017ce97eb36a849d64c080fb098dc99e200c37
JacksonB <jacksonbair@gmail.com>
2024-07-29 18:35:31 -0500

    Learning storage

---
+++ js/storage.js
@@ -0,0 +1,20 @@
+"use strict";
+function storeNote() {
+    const note = "pog";
+    chrome.storage.local.set({ egg: note }).then(() => {
+        console.log("Value is set");
+    });
+}
+function retrieveNote() {
+    chrome.storage.local.get(["key"]).then((result) => {
+        console.log("Value is " + result.key);
+    });
+    console.log("Value is retrieved");
+}
+const storeNoteButton = document.getElementById("storeNote");
+if (storeNoteButton) {
+    storeNoteButton.addEventListener("click", storeNote);
+}
+else {
+    console.error("Store Note button not found");
+}

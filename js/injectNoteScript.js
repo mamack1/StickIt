1f3cd7cd758b8e18885a5e931d56d27c3820a628
JacksonB <jacksonbair@gmail.com>
2024-08-17 12:01:45 -0500

    I am beginning to taste it. I know the process I need for storing notes. I am working through the process, and have begun the storing of the data,.

--- js/injectNoteScript.js
+++ js/injectNoteScript.js
@@ -5,76 +5,122 @@ console.log("injectNoteScript.ts loaded");
 //TODO: fix possible matching ids
 // Function to generate a unique ID for the note
 function generateUniqueId() {
-    return Math.random().toString(36).substr(2, 9);
+	return Math.random().toString(36).substr(2, 9);
 }
 // Function to create and display the note
 function createNewNote(noteData) {
-    const noteElement = createNoteElement(noteData);
-    document.body.appendChild(noteElement);
+	const noteElement = createNoteElement(noteData);
+	document.body.appendChild(noteElement);
 }
 // Function to create HTML element for the note
 function createNoteElement(noteData) {
-    const noteContainer = document.createElement("div");
-    noteContainer.innerHTML = noteData.innerhtml.trim();
-    const noteElement = noteContainer.firstChild;
-    noteElement.style.backgroundColor = noteData.color;
-    noteElement.style.top = `${noteData.position.top}px`;
-    noteElement.style.left = `${noteData.position.left}px`;
-    makeDraggable(noteElement);
-    setupCloseButton(noteElement);
-    return noteElement;
+	const noteHost = document.createElement("div");
+	noteHost.style.position = "absolute";
+	noteHost.style.top = `${noteData.position.top}px`;
+	noteHost.style.left = `${noteData.position.left}px`;
+	noteHost.style.width = "200px";
+	noteHost.style.height = "150px";
+	noteHost.style.zIndex = "2147483646";
+	const shadowRoot = noteHost.attachShadow({ mode: "open" });
+	const noteContent = document.createElement("div");
+	noteContent.innerHTML = noteData.innerhtml.trim();
+	noteContent.style.backgroundColor = noteData.color;
+	noteContent.style.padding = "10px";
+	noteContent.style.borderRadius = "5px";
+	noteContent.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.3)";
+	const handle = document.createElement("div");
+	handle.style.width = "50px";
+	handle.style.height = "5px";
+	handle.style.marginTop = "5px";
+	handle.style.marginBottom = "5px";
+	handle.style.backgroundColor = "grey";
+	handle.style.borderRadius = "10px";
+	handle.style.position = "absolute";
+	handle.style.top = "5px";
+	handle.style.left = "50%";
+	handle.style.transform = "translateX(-50%)";
+	handle.style.cursor = "grab";
+	handle.style.zIndex = "2147483647";
+	noteContent.appendChild(handle);
+	const textarea = noteContent.querySelector(".note-content");
+	textarea.style.width = "100%";
+	textarea.style.height = "100px";
+	textarea.style.backgroundColor = noteData.color;
+	textarea.style.border = "none";
+	textarea.style.resize = "none";
+	textarea.style.outline = "none";
+	textarea.style.color = "black";
+	const closeButton = noteContent.querySelector(".close-note");
+	closeButton.style.position = "absolute";
+	closeButton.style.top = "5px";
+	closeButton.style.right = "5px";
+	closeButton.style.backgroundColor = noteData.color;
+	closeButton.style.color = "black";
+	closeButton.style.border = "none";
+	closeButton.style.borderRadius = "50%";
+	closeButton.style.width = "20px";
+	closeButton.style.height = "20px";
+	closeButton.style.cursor = "pointer";
+	shadowRoot.appendChild(noteContent);
+	document.body.appendChild(noteHost);
+	setupCloseButton(noteHost);
+	makeDraggable(handle, noteHost);
+	return noteHost;
 }
-// Function to set up the close button for the note
-function setupCloseButton(noteElement) {
-    const closeButton = noteElement.querySelector(".close-note");
-    if (closeButton) {
-        closeButton.addEventListener("click", () => {
-            document.body.removeChild(noteElement);
-        });
-    }
+function setupCloseButton(noteHost) {
+	var _a;
+	const closeButton =
+		(_a = noteHost.shadowRoot) === null || _a === void 0
+			? void 0
+			: _a.querySelector(".close-note");
+	if (closeButton) {
+		closeButton.addEventListener("click", () => {
+			document.body.removeChild(noteHost);
+		});
+	}
 }
-// Function to make the note draggable
-function makeDraggable(element) {
-    let offsetX, offsetY;
-    let isDragging = false;
-    element.addEventListener("mousedown", (event) => {
-        offsetX = event.clientX - element.getBoundingClientRect().left;
-        offsetY = event.clientY - element.getBoundingClientRect().top;
-        isDragging = true;
-        element.style.cursor = "grabbing";
-    });
-    document.addEventListener("mousemove", (event) => {
-        if (isDragging) {
-            element.style.left = `${event.clientX - offsetX}px`;
-            element.style.top = `${event.clientY - offsetY}px`;
-        }
-    });
-    document.addEventListener("mouseup", () => {
-        isDragging = false;
-        element.style.cursor = "grab";
-    });
+function makeDraggable(handle, noteHost) {
+	let offsetX, offsetY;
+	let isDragging = false;
+	handle.addEventListener("mousedown", (event) => {
+		offsetX = event.clientX - noteHost.getBoundingClientRect().left;
+		offsetY = event.clientY - noteHost.getBoundingClientRect().top;
+		isDragging = true;
+		handle.style.cursor = "grabbing";
+	});
+	document.addEventListener("mousemove", (event) => {
+		if (isDragging) {
+			const newX = event.clientX - offsetX;
+			const newY = event.clientY - offsetY;
+			noteHost.style.left = `${newX}px`;
+			noteHost.style.top = `${newY}px`;
+		}
+	});
+	document.addEventListener("mouseup", () => {
+		isDragging = false;
+		handle.style.cursor = "grab";
+	});
 }
-// This function will be called by the background script when requested
-function handleCreateNoteRequest() {
-    let tempID = generateUniqueId();
-    const noteData = {
-        id: tempID,
-        color: "yellow",
-        position: { top: 100, left: 100 },
-        innerhtml: `
-            <div class="note" id="${tempID} "style="background-color: yellow; position: absolute; top: 100px; left: 100px; padding: 10px; border-radius: 5px; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);">
-                <textarea class="note-content" style="width: 100%; height: 100px; background-color: yellow; border: none; resize: none; outline: none; color: black;">New Note!!!!</textarea>
-                <button class="close-note" style="position: absolute; top: 5px; right: 5px; background-color: yellow; color: black; border: none; border-radius: 50%; width: 20px; height: 20px; cursor: pointer;">X</button>
+function handleCreateNoteRequest(color) {
+	const noteData = {
+		id: generateUniqueId(),
+		color: color,
+		position: { top: 100, left: 700 },
+		innerhtml: `
+            <div class="note" style="padding: 10px;">
+                <textarea class="note-content">New Note!!!!</textarea>
+                <button class="close-note">X</button>
             </div>
         `,
-    };
-    createNewNote(noteData);
+	};
+	createNewNote(noteData);
 }
 // Listen for createNote messages
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "createNote") {
-        handleCreateNoteRequest();
-        sendResponse({ success: true });
-        return true;
-    }
+	if (request.action === "createNote") {
+		handleCreateNoteRequest(request.color);
+		console.log("NOTE INJECTED");
+		sendResponse({ success: true });
+		return true;
+	}
 });
